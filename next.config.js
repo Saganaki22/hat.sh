f1b6671090e268fecbfb4904fe60b2d66f89eefb
drbaph <84208527+Saganaki22@users.noreply.github.com>
2025-11-20 11:45:42 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,12 +6,4 @@ module.exports = {
   images: {
     unoptimized: true,
   },
-  // Fix for GitHub Pages with custom domain
-  exportPathMap: async function () {
-    return {
-      '/': { page: '/' },
-      '/about': { page: '/about' },
-      '/generate-keys': { page: '/generate-keys' },
-    };
-  },
 }
